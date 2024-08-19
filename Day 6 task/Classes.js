75824561fc8e620aff0f1a91dc6d9e5d1160c832
teju <tejashreechandraprakash@gmail.com>
2024-08-19 21:53:12 +0530

    Commit

--- Day 6 task/Classes.js
+++ Day 6 task/Classes.js
@@ -4,19 +4,19 @@ class Person {
   location;
   
   constructor(name, age, location) {
-    this.name = name;
-    this.age = age;
-    this.location = location;
+      this.name = name;
+      this.age = age;
+      this.location = location;
   }
 
   selfIntro() {
-    console.log(`Hi! I'm ${this.name}. I am ${this.age} years old and from ${this.location}.`);
+      return `Hi! I'm ${this.name}. I am ${this.age} years old and from ${this.location}.`;
   }
 }
 
 const john = new Person("John", 54, "Chennai");
 
-john.selfIntro();
+document.getElementById("output").innerText = john.selfIntro();
 
 class Uber {
   baseFare;
@@ -26,22 +26,22 @@ class Uber {
   time;
 
   constructor(baseFare, costPerKm, costPerMinute, distance, time) {
-    this.baseFare = baseFare;
-    this.costPerKm = costPerKm;
-    this.costPerMinute = costPerMinute;
-    this.distance = distance;
-    this.time = time;
+      this.baseFare = baseFare;
+      this.costPerKm = costPerKm;
+      this.costPerMinute = costPerMinute;
+      this.distance = distance;
+      this.time = time;
   }
 
   calculatePrice() {
-    const distanceCost = this.distance * this.costPerKm;
-    const timeCost = this.time * this.costPerMinute;
-    const totalFare = this.baseFare + distanceCost + timeCost;
-    return totalFare;
+      const distanceCost = this.distance * this.costPerKm;
+      const timeCost = this.time * this.costPerMinute;
+      const totalFare = this.baseFare + distanceCost + timeCost;
+      return totalFare;
   }
 }
 
-
-const myRide = new Uber(50, 10, 2, 15, 30); 
+const myRide = new Uber(50, 10, 2, 15, 30);
 
 console.log(`The total fare for your Uber ride is ₹${myRide.calculatePrice()}.`);
+document.getElementById("output").innerText += `\nThe total fare for your Uber ride is ₹${myRide.calculatePrice()}.`;
